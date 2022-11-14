87abb6919eaccb14769ee30436e4fe5d55b4022f
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2022-11-14 08:52:18 +0530

    changes to js and ejs

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,11 @@ router.get('/new', (req, res) => {
     res.render('articles/new', {article: new Article() })
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', {article: article })
+})
+
 router.get('/:slug', async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug })
     if(article == null) {
@@ -14,38 +19,47 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article: article })
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        companyName: req.body.companyName,
-        jobTitle: req.body.jobTitle,
-        deadline: req.body.deadline,
-        stipend: req.body.stipend,
-        location: req.body.location,
-        duration: req.body.duration,
-        branches: req.body.branches,
-        formLink: req.body.formLink,
-        jobDescription: req.body.jobDescription
-    })
-    console.log(article)
-    try {
-        article = await article.save()
-        //res.send("Success!")
-        res.redirect(`/articles/${article.slug}`)
-        console.log('rediredcted to ${article.slug}')
-        // res.redirect(`./articles/${article._id}`)
-    }
-    catch (e) {
-        console.log('Error',e.message)
-        console.log(e.stack)
-        res.render('articles/new', { article: article}) //ensures prefilling in case of any errors
-    }
-    
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
 
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res)  => {
+        let article = req.article
+            article.companyName = req.body.companyName
+            article.jobTitle = req.body.jobTitle
+            article.deadline = req.body.deadline
+            article.stipend = req.body.stipend
+            article.location = req.body.location
+            article.duration = req.body.duration
+            article.branches = req.body.branches
+            article.formLink = req.body.formLink
+            article.jobDescription = req.body.jobDescription
+        //console.log(article)
+        try {
+            article = await article.save()
+            //res.send("Success!")
+            res.redirect(`/articles/${article.slug}`)
+            console.log('rediredcted to ${article.slug}')
+            // res.redirect(`./articles/${article._id}`)
+        }
+        catch (e) {
+            console.log('Error',e.message)
+            console.log(e.stack)
+            res.render(`articles/${path}`, { article: article}) //ensures prefilling in case of any errors
+        }
+    }
+}
 
 module.exports = router
